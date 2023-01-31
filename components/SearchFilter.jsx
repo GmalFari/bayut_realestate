58deac1241f6736abb3d16dac068076b45f76293
GmalFari <gmalfari926@gmail.com>
2023-01-31 13:47:14 +0300

    add useEffect to searchfilter

--- components/SearchFilter.jsx
+++ components/SearchFilter.jsx
@@ -7,21 +7,39 @@ import Image from 'next/image';
 
 import {filterData,getFilterValues} from "../utils/filterData";
 const SearchFilter = () => {
-  const [filters,setFilters] = useState(filterData);
+  const [filters] = useState(filterData);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [locationData, setLocationData] = useState();
+  const [showLocations, setShowLocations] = useState(false);
+  const [loading, setLoading] = useState(false);
   const router = useRouter();
+
   const searchProperties = (
     (filtervalues) =>{
       const path = router.pathname;
       const {query } = router;
       const values = getFilterValues(filtervalues);
       values.forEach((item)=>{
-        if(item.value && filtervalues?.[item.nameck]){
+        if(item.value && filtervalues?.[item.name]){
           query[item.name] = item.value
         }
       })
       router.push({pathname:path,query})
     }
+    
   )
+  useEffect(()=>{
+    if(searchTerm !== ''){
+      const fetchData = async ()=>{
+        setLoading(true);
+        const data = await fetchApi(`{baseUrl}/auto-complete?query=${searchTerm}`)
+        setLoading(data?.hits);
+      };
+      fetchData();
+    }
+  },[searchTerm]);
+  
+  
   return (
   <Flex bg="gray.100" p="4" justifyContent="center" flexWrap="wrap">
     {filters.map((filter)=>(
