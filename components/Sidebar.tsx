11de58c0109d923ff4c51bfb7cb0a05802b17719
GmalFari <gmalfari926@gmail.com>
2023-02-07 11:12:49 +0300

    a

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -198,8 +198,7 @@ const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
           <Menu>
             <MenuButton
               py={2}
-              transition="all 0.3s"
-              _focus={{ boxShadow: 'none' }}>
+              transition="all 0.3s">
               <HStack>
                 <Avatar
                   size={'sm'}
