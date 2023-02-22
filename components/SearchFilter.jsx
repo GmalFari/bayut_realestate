af3c0db9909b8c6c36e16de516e53d1e2801252d
GmalFari <72351012+GmalFari@users.noreply.github.com>
2023-02-22 19:40:48 +0300

    Update SearchFilter.jsx

--- components/SearchFilter.jsx
+++ components/SearchFilter.jsx
@@ -4,7 +4,6 @@ import {Flex,Select ,Box,Text,Input,Spinner,Icon,Button} from '@chakra-ui/react'
 import  { useRouter } from 'next/router';
 import {MdCancel} from 'react-icons/md';
 import Image from 'next/image';
-
 import {filterData,getFilterValues} from "../utils/filterData";
 const SearchFilter = () => {
   const [filters] = useState(filterData);
