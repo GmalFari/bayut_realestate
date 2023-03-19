009fdf9314703747a8c7341b357ee4b3a6e6d98d
GmalFari <72351012+GmalFari@users.noreply.github.com>
2023-03-19 17:17:44 +0300

    Update HorizonalCard.jsx

--- components/HorizonalCard.jsx
+++ components/HorizonalCard.jsx
@@ -14,7 +14,8 @@ import MainBtn from "./MainBtn";
 import Horizontal from './Horizontal';
 const HorizonalCard = (
     {property:{coverPhoto,price,rentFrequency,location,rooms,title_l1,baths,area,agency,isVerified,externalID,geography}}) => {
-      const loc1 = location[3]['name_l1'].replace('Dubai','Sanaa');
+      const loc11 = location[3]['name_l1']
+      const loc1 = loc11.replace('Dubai','Sanaa');
       return (
       <Flex w={['95%','95%','700px','800px']} 
             height={[170,200]} 
@@ -49,7 +50,7 @@ const HorizonalCard = (
                 </Text>
                 <Flex isTruncated flexWrap='noWrap' display={'flex'}  pt={[1,2]} fontSize={['11px','sm','md']} color={'#006169'} >
                 <BsFillGeoAltFill />
-                {`${}`},
+                {`${loc1}`},
                 {`${location[2]['name_l1']}`},
                 {`${location[1]['name_l1']}`}
                 </Flex>
